c8a01e7aa0e52b23b568df616358cd0df75f743d
jackssrt <33695699+jackssrt@users.noreply.github.com>
2021-12-06 15:02:14 +0200

    Fix circular import

--- modules/logs.py
+++ modules/logs.py
@@ -1,6 +1,6 @@
 import logging
 from discord import Webhook, RequestsWebhookAdapter
-from modules.env import env
+
 
 LOG_FORMAT = "%(asctime)s [%(levelname)s] - %(message)s"
 logging.basicConfig(filename="debug.log", level=logging.INFO, format=LOG_FORMAT)
@@ -8,5 +8,7 @@ logger = logging.getLogger()
 
 
 def discordlog(string: str):
+    from modules.env import env
+
     webhook = Webhook.from_url(env.FGN_DISCORD_LOG, adapter=RequestsWebhookAdapter())
     webhook.send(string)
